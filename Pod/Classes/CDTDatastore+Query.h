7883bb89c3722e71cffb820b27ce772af81874d4
Michael Rhodes <mike.rhodes@gmail.com>
2014-11-21 11:21:36 +0000

    Basic documentation for the Query category

--- Pod/Classes/CDTDatastore+Query.h
+++ Pod/Classes/CDTDatastore+Query.h
@@ -17,19 +17,85 @@
 #import "CDTDatastore.h"
 #import "CDTQIndexManager.h"
 
+/**
+ This category adds query capabilities to CDTDatastore.
+ 
+ The query interface is based on Cloudant Query, which has a query
+ syntax loosely modelled on MongoDB's query selector.
+ */
 @interface CDTDatastore (Query)
 
+/**
+ Return a list of the indexes defined.
+ 
+ This is returned in a dictionary structure:
+ 
+     { "indexName": { "fields": [ "field1", "field2" ],
+                      "type": "json",
+                      "name": "indexName" },
+       ...
+     }
+ */
 - (NSDictionary * /* NSString -> NSArray[NSString]*/)listIndexes;
 
+/**
+ Create a new index over a set of fields.
+ 
+ Fields in sub-documents can be specified using dotted notation.
+ 
+ An example:
+ 
+     { "name": "mike", "address": { "street": "Any Road" } }
+ 
+ The field name "name" would index "mike", while the field name
+ "address.street" would index "Any Road".
+ 
+ Indexing an array will add each value in the array to the index.
+ There may only be one array field per index.
+ 
+ @return The name of the index if it's created successfully.
+ */
 - (NSString *)ensureIndexed:(NSArray * /* NSString */)fieldNames
                    withName:(NSString *)indexName;
 
+/**
+ Delete an index.
+ */
 - (BOOL)deleteIndexNamed:(NSString *)indexName;
 
+/**
+ Bring indexes up to date with the contents of the datastore.
+ 
+ This happens automatically before queries happen, but if you
+ know there are a lot of updates to process, explicitly calling
+ -updateAllIndexes will save time during the next query. It's
+ useful to call when a replication completes, for example.
+ */
 - (BOOL)updateAllIndexes;
 
+/**
+ Find documents matching a query.
+ 
+ See -find:skip:limit:fields:sort: for more details.
+ 
+ Failures during query (e.g., invalid query) are logged rather than
+ error being returned.
+ 
+ @return Set of documents, or `nil` if there was an error.
+ */
 - (CDTQResultSet *)find:(NSDictionary *)query;
 
+/**
+ Find document matching a query.
+ 
+ See https://github.com/cloudant/CloudantQueryObjc for details of the
+ query syntax and option meanings.
+ 
+ Failures during query (e.g., invalid query) are logged rather than
+ error being returned.
+ 
+ @return Set of documents, or `nil` if there was an error.
+ */
 - (CDTQResultSet *)find:(NSDictionary *)query
                    skip:(NSUInteger)skip
                   limit:(NSUInteger)limit
